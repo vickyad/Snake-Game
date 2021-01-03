9b102d782174d7509bdcfd9aa484608753421e94
Victoria <victoria.ad40@gmail.com>
2021-01-03 10:19:47 -0300

    Feat ( add movement by finger move )

--- public/input.js
+++ public/input.js
@@ -1,6 +1,65 @@
 let inputDirection = {x: 0, y: 0, d:'N'}
 let lastInputDirection = {x: 0, y: 0, d:'N'}
 
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+}                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            if(lastInputDirection.x === 0){
+                inputDirection = {x: -1, y: 0, d:'L'}
+            }
+            /* left swipe */ 
+        } else {
+            if(lastInputDirection.x === 0){
+                inputDirection = {x: 1, y: 0, d:'R'}
+            }
+            /* right swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            if(lastInputDirection.y === 0){
+                inputDirection = {x: 0, y: -1, d:'U'}
+            }
+            /* up swipe */ 
+        } else {
+            if(lastInputDirection.y === 0){
+                inputDirection = {x:0, y: 1, d:'D'}
+            } 
+            /* down swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+}
+
+
 window.addEventListener('keydown', e => {
     switch(e.key) {
         case 'ArrowUp':
